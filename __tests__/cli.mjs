2c8e129884b2d0b80442b0549cef420441d219cc
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2022-04-02 15:23:30 -0400

    Add cli version and help parameter tests
    Improves coverage
    
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- __tests__/cli.mjs
+++ __tests__/cli.mjs
@@ -4,6 +4,10 @@ import ava from 'ava';
 import {execa as execa} from 'execa';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+
+const requireShim = createRequire(import.meta.url);
+const versionString = requireShim('../package.json').version;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const fixture = path.resolve(path.join(__dirname, './fixtures/nested.fixture.css'));
@@ -11,6 +15,19 @@ const fixture = path.resolve(path.join(__dirname, './fixtures/nested.fixture.css
 function setup (args) {
     return execa(path.resolve(path.join(__dirname, '../bin/cmd.js')), args, {stripEof: false, cwd: __dirname, stripFinalNewline: false});
 }
+
+ava('cli: version', t => {
+    return setup(['--version']).then(({stdout}) => {
+        t.deepEqual(stdout, versionString + '\n', 'should report the package version');
+    });
+});
+
+
+ava('cli: help', t => {
+    return setup(['--help']).then(({stdout}) => {
+        t.deepEqual(stdout, read(path.resolve(path.join(__dirname, './../bin/usage.txt')), 'utf-8'), 'should report the help (usage) text');
+    });
+});
 
 ava('cli: defaults', t => {
     return setup([fixture]).then(({stdout}) => {
